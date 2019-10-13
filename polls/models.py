a74f17dc238786e43d6269cb45b83f5394f7a708
pjt3591oo <pjt3591oo@gmail.com>
2019-10-13 11:10:51 +0900

    admin에 polls/models.py 에 정의한 모델 추가

--- polls/models.py
+++ polls/models.py
@@ -1,12 +1,25 @@
+import datetime # django.utils.timezone에서 참조하기 위해 추가
+
 from django.db import models
+from django.utils import timezone
+
+
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+  question_text = models.CharField(max_length=200)
+  pub_date = models.DateTimeField('date published')
+  
+  def __str__(self):
+      return self.question_text
 
+  def was_published_recently(self):
+    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    votess = models.IntegerField(default=0)
+  question = models.ForeignKey(Question, on_delete=models.CASCADE)
+  choice_text = models.CharField(max_length=200)
+  votes = models.IntegerField(default=0)
+  votess = models.IntegerField(default=0)
+  
+  def __str__(self):
+    return self.choice_text
